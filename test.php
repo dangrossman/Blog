6dbe4c2a2cb09ee8288eae20e2fedf98e3845ee3
Dan Grossman <dan@awio.com>
2012-08-21 02:14:11 -0400

    Updates

--- test.php
+++
@@ -1,9 +0,0 @@
-<?php
-function contains_letters($word1, $word2) {
-  for ($i = 0; $i < strlen($word1); $i++)
-    if (strpos($word2, $word1{$i}) === false)
-      return false;
-  return true;
-}
-
-var_dump(contains_letters('abcde', 'abdefg'));
