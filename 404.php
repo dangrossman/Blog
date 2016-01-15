f423fd19ad8867cf0275c7ba94f0ac212c8f71df
Dan Grossman <dan@awio.com>
2016-01-14 23:24:23 -0500

    Add 404 file

---
+++ 404.php
@@ -0,0 +1,26 @@
+<?php get_header(); ?>
+
+<div class="container">
+
+  <div class="row">
+
+    <div class="col-lg-12 col-xs-12 bigcol">
+
+        <ul class="pager" id="backhome">
+          <li class="previous"><a href="/">&larr; Home</a></li>
+        </ul>
+
+        <div class="post">
+	  <h2>Nothing to see here.</h2>
+	  <p>Really, there's nothing to see here. Whatever you were hoping to find doesn't exist any more.</p>
+        </div>
+
+
+    </div><!-- //content column -->
+
+
+  </div><!-- //row -->
+
+</div><!-- //container -->
+
+<?php get_footer(); ?>
